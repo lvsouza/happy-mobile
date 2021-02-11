64bdcfb46f6dce9ffc0107f60cfb23bc86d38273
lvargas <lvargas@gvdasa.com.br>
2021-02-11 08:02:46 -0300

    feat: added maps page
    Added expo google fonts
    Used expo icons
    Added MapView
    Added map marker
    Added map marker callout

--- App.tsx
+++ App.tsx
@@ -1,12 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+import { StyleSheet, View, Dimensions, Text, TouchableOpacity } from 'react-native';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+
+import { Images } from './src/images';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Nunito_800ExtraBold,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+          latitude: -29.6760828,
+          longitude: -51.0845638,
+        }}
+      >
+        <Marker
+          icon={Images.mapMarker}
+          coordinate={{
+            latitude: -29.6760828,
+            longitude: -51.0845638,
+          }}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8
+          }}
+        >
+          <Callout tooltip onPress={() => { }}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>
+                Lar de testes
+              </Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+
+        <TouchableOpacity style={styles.createOrphanageButton}>
+          <Feather name="plus" color="white" size={20} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -17,5 +67,48 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    borderRadius: 16,
+    paddingHorizontal: 16,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(255,255,255,0.8)',
+  },
+  calloutText: {
+    fontSize: 14,
+    color: '#0089a5',
+    fontFamily: 'Nunito_700Bold',
+  },
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#15c3d6'
+  },
+  footer: {
+    left: 24,
+    right: 24,
+    bottom: 32,
+    height: 56,
+    elevation: 3,
+    paddingLeft: 24,
+    borderRadius: 20,
+    position: 'absolute',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+  },
+  footerText: {
+    fontFamily: 'Nunito_700Bold',
+    color: '#8fa7b3',
   },
 });
