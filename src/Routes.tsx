8660a5092f3b0ee08f5f5ef9a5ac79573df4d869
lvargas <lvargas@gvdasa.com.br>
2021-02-11 13:26:46 -0300

    feat: added to routes all new pages

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,16 +2,46 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { OrphanagesMap, OrphanagesDetails } from './pages';
+import { OrphanagesMap, OrphanagesDetails, SelectMapPosition, CreateOrphanage } from './pages';
+import { CustomHeader } from './components';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export const Routes: React.FC = () => {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="OrphanagesMap" component={OrphanagesMap} />
-        <Screen name="OrphanagesDetails" component={OrphanagesDetails} />
+      <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
+        <Screen
+          name="OrphanagesMap"
+          component={OrphanagesMap}
+        />
+
+        <Screen
+          name="CreateOrphanage"
+          component={CreateOrphanage}
+          options={{
+            headerShown: true,
+            header: () => <CustomHeader title="Informe os dados" />
+          }}
+        />
+
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <CustomHeader title="Selecione no mapa" showCancel={false} />
+          }}
+        />
+
+        <Screen
+          name="OrphanagesDetails"
+          component={OrphanagesDetails}
+          options={{
+            headerShown: true,
+            header: () => <CustomHeader title="Orfanato" />
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
+export * from './select-map-position/SelectMapPosition';
 export * from './orphanages-details/OrphanagesDetails';
+export * from './create-orphanage/CreateOrphanage';
 export * from './orphanages-map/OrphanagesMap';
