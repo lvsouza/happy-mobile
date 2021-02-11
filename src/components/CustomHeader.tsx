a05f5971980f320a9d21417c39feab0257a088d4
lvargas <lvargas@gvdasa.com.br>
2021-02-11 13:23:50 -0300

    feat: added custom header component

---
+++ src/components/CustomHeader.tsx
@@ -0,0 +1,53 @@
+import React, { useCallback } from "react";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, StyleSheet } from "react-native";
+import { Feather } from '@expo/vector-icons';
+
+interface ICustomHeaderProps {
+  title: string;
+  showCancel?: boolean;
+}
+export const CustomHeader: React.FC<ICustomHeaderProps> = ({ title, showCancel = true }) => {
+  const { goBack, navigate } = useNavigation();
+
+  const handleNavigateToOrphanageMap = useCallback(() => {
+    navigate('OrphanagesMap');
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <BorderlessButton onPress={goBack}>
+        <Feather name="arrow-left" size={24} color="#15b6d6" />
+      </BorderlessButton>
+
+      <Text style={styles.text}>{title}</Text>
+
+      {showCancel &&
+        <BorderlessButton onPress={handleNavigateToOrphanageMap} >
+          <Feather name="x" size={24} color="#15b6d6" />
+        </BorderlessButton>
+      }
+
+      {!showCancel && <View />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    paddingTop: 44,
+    borderWidth: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderColor: '#dde3f0',
+    backgroundColor: '#f9fafc',
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+    color: '#8fa7b3',
+    fontFamily: 'Nunito_600SemiBold',
+  }
+});
